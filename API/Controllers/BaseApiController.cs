f8fb11f8f7fa3c6d0b50e9ab0c549deb160f56b5
Nishanth Thanayil <nishanth@nishanths-mbp.kpn>
2023-01-13 06:38:10 +0100

    added auto mapper

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MediatR;
 
 namespace API.Controllers
 {
@@ -10,6 +11,8 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseApiController: ControllerBase
     {
-        
+        private IMediator _mediator;
+        //Derived classes can access this
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>(); 
     }
 }
