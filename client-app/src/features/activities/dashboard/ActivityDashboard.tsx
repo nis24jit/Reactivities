27218e40a0d1daa6b819c018051d539e92d36d7a
Nishanth Thanayil <nishanth@nishanths-mbp.kpn>
2023-01-20 06:37:12 +0100

    feat: added axios and type info

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,59 +1,64 @@
 import React from 'react';
-import { Grid } from 'semantic-ui-react';
-import { Activity } from '../../../app/layout/models/activity';
+import {Grid} from 'semantic-ui-react';
+import {Activity} from '../../../app/layout/models/activity';
 import ActivityList from './ActivityList';
 import ActivityDetails from '../details/ActivityDetails';
 import ActivityForm from '../form/ActivityForm';
-import { v4 as uuid } from 'uuid';
+import {v4 as uuid} from 'uuid';
 
 interface Props {
-  activities: Activity[];
-  selectedActivity: Activity | undefined;
-  selectActivity: (id: string) => void;
-  cancelSelectActivity: () => void;
-  editMode: boolean;
-  openForm: (id: string) => void;
-  closeForm: () => void;
-  createOrEdit: (activity: Activity) => void;
-  deleteActivity: (id: string) => void;
+    activities: Activity[];
+    selectedActivity: Activity | undefined;
+    selectActivity: (id: string) => void;
+    cancelSelectActivity: () => void;
+    editMode: boolean;
+    openForm: (id: string) => void;
+    closeForm: () => void;
+    createOrEdit: (activity: Activity) => void;
+    deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({
-  activities,
-  selectActivity,
-  selectedActivity,
-  cancelSelectActivity,
-  editMode,
-  openForm,
-  closeForm,
-  createOrEdit,
-  deleteActivity,
-}: Props) {
-  return (
-    <Grid>
-      <Grid.Column width='10'>
-        <ActivityList
-          activities={activities}
-          selectActivity={selectActivity}
-          deleteActivity={deleteActivity}
-        ></ActivityList>
-      </Grid.Column>
-      <Grid.Column width='6'>
-        {selectedActivity && !editMode && (
-          <ActivityDetails
-            activity={selectedActivity}
-            cancelSelectActivity={cancelSelectActivity}
-            openForm={openForm}
-          ></ActivityDetails>
-        )}
-        {editMode && (
-          <ActivityForm
-            closeForm={closeForm}
-            activity={selectedActivity}
-            createOrEdit={createOrEdit}
-          ></ActivityForm>
-        )}
-      </Grid.Column>
-    </Grid>
-  );
+                                              activities,
+                                              selectActivity,
+                                              selectedActivity,
+                                              cancelSelectActivity,
+                                              editMode,
+                                              openForm,
+                                              closeForm,
+                                              createOrEdit,
+                                              deleteActivity,
+                                              submitting,
+
+                                          }: Props) {
+    return (
+        <Grid>
+            <Grid.Column width='10'>
+                <ActivityList
+                    activities={activities}
+                    selectActivity={selectActivity}
+                    deleteActivity={deleteActivity}
+                    submitting={submitting}
+                ></ActivityList>
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {selectedActivity && !editMode && (
+                    <ActivityDetails
+                        activity={selectedActivity}
+                        cancelSelectActivity={cancelSelectActivity}
+                        openForm={openForm}
+                    ></ActivityDetails>
+                )}
+                {editMode && (
+                    <ActivityForm
+                        closeForm={closeForm}
+                        activity={selectedActivity}
+                        createOrEdit={createOrEdit}
+                        submitting={submitting}
+                    ></ActivityForm>
+                )}
+            </Grid.Column>
+        </Grid>
+    );
 }
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,85 +1,91 @@
-import React, { ChangeEvent, useState } from 'react';
-import { Button, Form, Segment } from 'semantic-ui-react';
-import { Activity } from '../../../app/layout/models/activity';
+import React, {ChangeEvent, useState} from 'react';
+import {Button, Form, Segment} from 'semantic-ui-react';
+import {Activity} from '../../../app/layout/models/activity';
 
 interface Props {
-  activity: Activity | undefined;
-  closeForm: () => void;
-  createOrEdit: (activity: Activity) => void;
+    activity: Activity | undefined;
+    closeForm: () => void;
+    createOrEdit: (activity: Activity) => void;
+    submitting: boolean;
 }
 
 export default function ActivityForm({
-  activity: selectedActivity,
-  closeForm,
-  createOrEdit,
-}: Props) {
-  const initialState = selectedActivity ?? {
-    id: '',
-    title: '',
-    category: '',
-    description: '',
-    date: '',
-    city: '',
-    venue: '',
-  };
-  const [activity, setActivity] = useState(initialState);
+                                         activity: selectedActivity,
+                                         closeForm,
+                                         createOrEdit,
+                                         submitting
+                                     }: Props) {
+    const initialState = selectedActivity ?? {
+        id: '',
+        title: '',
+        category: '',
+        description: '',
+        date: '',
+        city: '',
+        venue: '',
+    };
+    const [activity, setActivity] = useState(initialState);
 
-  function handleSubmit() {
-    createOrEdit(activity);
-  }
-  function handleInputChange(
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) {
-    const { name, value } = event.target;
-    setActivity({ ...activity, [name]: value });
-  }
-  return (
-    <Segment clearing>
-      <Form onSubmit={handleSubmit} autoComplete='off'>
-        <Form.Input
-          placeholder='Title'
-          value={activity.title}
-          onChange={handleInputChange}
-          name='title'
-        ></Form.Input>
-        <Form.TextArea
-          placeholder='Description'
-          value={activity.description}
-          onChange={handleInputChange}
-          name='description'
-        ></Form.TextArea>
-        <Form.Input
-          placeholder='Category'
-          value={activity.category}
-          onChange={handleInputChange}
-          name='category'
-        ></Form.Input>
-        <Form.Input
-          placeholder='Date'
-          value={activity.date}
-          onChange={handleInputChange}
-          name='date'
-        ></Form.Input>
-        <Form.Input
-          placeholder='City'
-          value={activity.city}
-          onChange={handleInputChange}
-          name='city'
-        ></Form.Input>
-        <Form.Input
-          placeholder='Venue'
-          value={activity.venue}
-          onChange={handleInputChange}
-          name='venue'
-        ></Form.Input>
-        <Button
-          floated='right'
-          positive
-          type='submit'
-          content='Submit'
-        ></Button>
-        <Button onClick={closeForm} floated='right' content='Cancel'></Button>
-      </Form>
-    </Segment>
-  );
+    function handleSubmit() {
+        createOrEdit(activity);
+    }
+
+    function handleInputChange(
+        event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) {
+        const {name, value} = event.target;
+        setActivity({...activity, [name]: value});
+    }
+
+    return (
+        <Segment clearing>
+            <Form onSubmit={handleSubmit} autoComplete='off'>
+                <Form.Input
+                    placeholder='Title'
+                    value={activity.title}
+                    onChange={handleInputChange}
+                    name='title'
+                ></Form.Input>
+                <Form.TextArea
+                    placeholder='Description'
+                    value={activity.description}
+                    onChange={handleInputChange}
+                    name='description'
+                ></Form.TextArea>
+                <Form.Input
+                    placeholder='Category'
+                    value={activity.category}
+                    onChange={handleInputChange}
+                    name='category'
+                ></Form.Input>
+                <Form.Input
+                    type='date'
+                    placeholder='Date'
+                    value={activity.date}
+                    onChange={handleInputChange}
+                    name='date'
+                ></Form.Input>
+                <Form.Input
+                    placeholder='City'
+                    value={activity.city}
+                    onChange={handleInputChange}
+                    name='city'
+                ></Form.Input>
+                <Form.Input
+                    placeholder='Venue'
+                    value={activity.venue}
+                    onChange={handleInputChange}
+                    name='venue'
+                ></Form.Input>
+                <Button
+                    loading={submitting}
+                    floated='right'
+                    positive
+                    type='submit'
+                    content='Submit'
+                ></Button>
+                <Button onClick={closeForm} floated='right' content='Cancel'></Button>
+            </Form>
+        </Segment>
+    );
 }
